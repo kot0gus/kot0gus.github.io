046bd0a7c73fb94635c42cfca581fb9fb3c96387
kot0gus <darja.rizkova07@gmail.com>
2025-06-18 10:43:15 +0300

    f

--- script1.js
+++ script1.js
@@ -11,7 +11,7 @@ const translations = {
         blog_title: "Blogs",
         bio_title: "Biogrāfija",
         gallery_title: "Galerija",
-        project_text: "Projekts apraksts: Praktiskajas macības laikā man vajadzēja uztaisīt cenu kalkulatoru mājaslapu izstrādes uzņēmumam. Galvenie nosācijumi bija izmantot 3 programmēšanas valodas HTML, CSS, JavaScript, arī vajadzēja izmantot checkbox.Vēl bija nepieciešams parbaudīt ka strada izveidota saite divas online programmas.Lai saite izskatījas labāk, mans uzdevums bija uztaisīt tabulas, kur atrodas visa nepieciešama informācija, lai varētu viegli izpētit, bez grutībam izmantot.",
+        project_text: "Projekts 1 - apraksts",
         bio_text: "Es esmu Darja Rižkova, RTK audzeknis programmēšanā. Turpmāk iešu macīties par programmēšanas lingvistu. Uz doto momentu zinu angļu, krievu, latviešu valodu un mācu vācu valodu. Man patīk ceļot un uz doto momentu biju apmeklējusi 22 valsti (mēs ar vēcakiem ceļojām ar mašinu ). Ka arī man ir suns viņu sauc Terra un viņai ir seši gadi. "
     },
     en: {
@@ -23,7 +23,7 @@ const translations = {
         blog_title: "Blog",
         bio_title: "Biography",
         gallery_title: "Gallery",
-        project_text: "Project description:",
+        project_text: "Project description",
         bio_text: "I'm Daria Ryzkhova, studying programming in RTK. In the future, I will study to programming linguist. At the moment, I know only three languages English, Russian and Latvian, but, also, I have been learning German for a half of year. I really enjoy traveling so I have visited 22 countries already(me with parents love traveling by car). Also, I have a dog her name is Terra and she is six years old."
 
     },
@@ -36,7 +36,7 @@ const translations = {
         blog_title: "Блог",
         bio_title: "Биография",
         gallery_title: "Галерея",
-        project_text:"Описание проекта: Во время практики в колледже, мне надо было создать калькулятор цен для компании по разработке сайтов. Главные критерии были использовать три языка программирования HTML, CSS, JavaScript, и использовать checkbox.Также было необходимо проверить как работает сайт через две онлайн программы. ПО мимо этого, я создала таблицы, чтобы было легче ориентироваться по сайту и, чтобы все необходимую информацию было легко найти.",
+        project_text:"Описание проекта",
         bio_text:"Меня зовут Дарья Рыжкова, я учусь в РТК по специальности программиста. В будущем я буду учиться на лингвиста и по профессии лингвист по программированию. На данный момент я знаю английский, русский, латышский и изучаю немецкий. Мне нравится путешествовать и на данный момент я посетила 22 страны(мы с родителями путешествуем на машине). Также, у меня есть собака её зовут Терра и ей шесть лет. "
     },
     de: {
