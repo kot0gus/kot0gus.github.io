74c9f2f0f8b156b36ddd94d68a9bdc64fef5226b
kot0gus <darja.rizkova07@gmail.com>
2025-06-18 09:54:45 +0300

    Update script1.js

--- script1.js
+++ script1.js
@@ -23,7 +23,7 @@ const translations = {
         blog_title: "Blog",
         bio_title: "Biography",
         gallery_title: "Gallery",
-        project_text: "Project description: During my college practice, I had to create a price calculator for a website development company. The main criteria were to use three programming languages ​​HTML, CSS, JavaScript, and use a checkbox. It was also necessary to check how the site works through two online programs. In addition to this, I created tables to make it easier to navigate the site and to make all the necessary information easy to find.",
+        project_text: "Project description:",
         bio_text: "I'm Daria Ryzkhova, studying programming in RTK. In the future, I will study to programming linguist. At the moment, I know only three languages English, Russian and Latvian, but, also, I have been learning German for a half of year. I really enjoy traveling so I have visited 22 countries already(me with parents love traveling by car). Also, I have a dog her name is Terra and she is six years old."
 
     },
@@ -48,7 +48,7 @@ const translations = {
       blog_title: "Blog",
       bio_title: "Biographie",
       gallery_title: "Galerie",
-      project_text: "Beschreibung des Projekts:",
+      project_text: "Beschreibung des Projekts",
       bio_text: "Mein Name ist Daria Ryzhkova, ich studiere am RTC und spezialisiere mich auf Programmierung. In Zukunft werde ich Linguistin studieren und von Beruf Linguistin für Programmierung sein. Im Moment ich spreche Englisch, Russisch, Lettisch und ein bisschen Deutsch.  Ich reise gerne und habe bisher 22 Länder besucht (meine Eltern und ich sind mit dem Auto unterwegs). So ich habe hund sie Name ist Terra und sie ist sechs Jāhre alt"
 
     }
