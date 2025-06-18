a77d5c0191f8ad6c98c7572d4872884e5bfd656b
kot0gus <darja.rizkova07@gmail.com>
2025-06-18 09:27:01 +0300

    n

--- script1.js
+++ script1.js
@@ -1,4 +1,5 @@
 document.getElementById('year').textContent = new Date().getFullYear();
+document.getElementById("Projekti").innerHTML = translations["ru"].project_text;
 
 const translations = {
     lv: {
